5404cd22c85b70e9aeea49db5c9ae3d46d111221
choukskander <skander.chouk@esprit.tn>
2024-08-22 14:13:17 +0100

    consommation resume

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -10,6 +10,14 @@ router.get('/', protect, getApplications);
 router.get('/:id', protect, getApplicationById);
 
 // Add new route for creating application (with multer middleware)
-router.post('/:jobId', protect, upload.single('resume'), createApplication);
-
+// router.post('/:jobId', protect, upload.single('resume'), createApplication);
+router.post('/:jobId', protect, (req, res, next) => {
+    upload.single('resume')(req, res, (err) => {
+      if (err) {
+        // Handle Multer errors
+        return res.status(400).json({ message: err.message });
+      }
+      next();
+    });
+  }, createApplication);
 module.exports = router;
